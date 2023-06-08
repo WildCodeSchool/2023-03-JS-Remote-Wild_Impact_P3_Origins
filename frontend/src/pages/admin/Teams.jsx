17a3818381c59348221ef4115002ccb51f3a4925
Robin31 <robindenis@hotmail.fr>
2023-06-08 16:46:37 +0200

    Récupération de la route Get

--- frontend/src/pages/admin/Teams.jsx
+++ frontend/src/pages/admin/Teams.jsx
@@ -1,15 +1,22 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import TeamsCard from "../../components/TeamsCard";
 
 function Teams() {
-  // const [teams, setTeams] = useState([]);
+  const [teams, setTeams] = useState([]);
 
   useEffect(() => {
     fetch(`${import.meta.env.VITE_BACKEND_URL}/teams`)
       .then((res) => res.json())
-      .then((data) => console.info(data))
+      .then((data) => setTeams(data))
       .catch((err) => console.error(err));
   }, []);
-  return <div>Teams</div>;
+  return (
+    <div className="Bloc1">
+      {teams.map((team) => (
+        <TeamsCard team={team} />
+      ))}
+    </div>
+  );
 }
 
 export default Teams;
