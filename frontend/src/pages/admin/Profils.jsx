90c760b70fc8d874e1d78c3fcbdac4c84cffe48f
Lucas DEGRET <lucas.degret@outlook.fr>
2023-06-08 16:45:17 +0200

    Stockage des profils dans un state + conso de la data

--- frontend/src/pages/admin/Profils.jsx
+++ frontend/src/pages/admin/Profils.jsx
@@ -1,15 +1,22 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 function Profils() {
+  const [profils, setProfils] = useState([]);
   useEffect(() => {
     fetch(`${import.meta.env.VITE_BACKEND_URL}/profils`)
       .then((res) => res.json())
-      .then((data) => console.info(data))
+      .then((data) => setProfils(data))
       .catch((err) => console.error(err));
   }, []);
   return (
     <main>
       <h1>User's Profils</h1>
+      {profils.map((profil) => (
+        <h2 key={profil.id}>
+          {" "}
+          {profil.firstname} {profil.lastname}{" "}
+        </h2>
+      ))}
     </main>
   );
 }
