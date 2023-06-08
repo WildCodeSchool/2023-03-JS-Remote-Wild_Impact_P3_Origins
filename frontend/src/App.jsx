cb5b5390a61ae86992811a9ebb8466f554b850db
Lucas DEGRET <lucas.degret@outlook.fr>
2023-06-08 10:35:37 +0200

    Mise place + importation du Formulaire dans admin layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserLayout from "./layout/UserLayout";
 import AdminLayout from "./layout/AdminLayout";
+import FormulaireExample from "./pages/admin/FormulaireExample";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<UserLayout />} />
-        <Route path="/admin" element={<AdminLayout />} />
+
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route path="" element={<FormulaireExample />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
