e470e4f2080e39507a2689e05b7e84d084d71889
Lucas DEGRET <lucas.degret@outlook.fr>
2023-07-12 10:35:08 +0200

    Utilisation du connexion.js pour fetch les datas + importation de toastify

--- frontend/src/pages/admin/Profils.jsx
+++ frontend/src/pages/admin/Profils.jsx
@@ -1,22 +1,44 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { toast, ToastContainer, Flip } from "react-toastify";
+import connexion from "../../services/connexion";
+import "react-toastify/dist/ReactToastify.css";
 
 function Profils() {
   const [profils, setProfils] = useState([]);
+
+  const getProfils = async () => {
+    const profilsData = await connexion.get("/profils");
+    try {
+      if (profilsData) {
+        setProfils(profilsData);
+      }
+    } catch (error) {
+      toast.error("Une erreur est survenue");
+    }
+  };
+
   useEffect(() => {
-    fetch(`${import.meta.env.VITE_BACKEND_URL}/profils`)
-      .then((res) => res.json())
-      .then((data) => setProfils(data))
-      .catch((err) => console.error(err));
+    getProfils();
   }, []);
+
   return (
     <main>
       <h1>User's Profils</h1>
       {profils.map((profil) => (
-        <h2 key={profil.id}>
-          {" "}
-          {profil.firstname} {profil.lastname}{" "}
-        </h2>
+        <Link to={`/admin/profils/${profil.id}`}>
+          <h2 key={profil.id}>
+            {profil.firstname} {profil.lastname}
+          </h2>
+        </Link>
       ))}
+      <ToastContainer
+        autoClose={2000}
+        position="top-center"
+        draggable
+        transition={Flip}
+        toastClassName="custom-toast"
+      />
     </main>
   );
 }
