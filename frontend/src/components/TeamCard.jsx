f75282d6f7ef037eec62cbc803b2ec26b40e3985
Lucas DEGRET <lucas.degret@outlook.fr>
2023-07-12 10:40:30 +0200

    Suppression de la function delete

--- frontend/src/components/TeamCard.jsx
+++ frontend/src/components/TeamCard.jsx
@@ -3,7 +3,6 @@ import PropTypes from "prop-types";
 import TeamType from "../Types/team";
 
 function TeamsCard({ team }) {
-
   return (
     <div className="Bloc2">
       <h2 className="titre">{team.name}</h2>
